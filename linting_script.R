62275d78a548fbf614e4f67003e5c9a3bfdaff5e
fna106 <151074097+fna106@users.noreply.github.com>
2026-05-04 13:30:55 -0400

    Testing the project file using lint functions

--- linting_script.R
+++ linting_script.R
@@ -7,7 +7,9 @@ library(lintr)
 ### Uncomment the following line and update the file path to the file you want
 ### to check.
 
-# lint(filename = "linting_script.R")
+lint(filename = "Mushroom_project.qmd")
+
+#It seems that the only problems lint is flagging this far are exceeding 80 characters and nameing style camelCase. I'm Happy with this!
 
 ## Linting a whole directory ----
 ### Uncomment the following line; this will check all files in your current (project) directory.
